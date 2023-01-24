ceddf5dfe36fb38680a8d9ae66169c6b5cdd71c7
Idel Fazletdinov <fvi-it@mail.ru>
2023-01-24 10:00:21 +0300

    телегам бот для игры камень-ножницы-бумага

---
+++ config_data/config.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from environs import Env
+from typing import Optional
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: Optional[str] = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
