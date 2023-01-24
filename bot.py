ddd3eb00517238f4b24f9a99f73c15bf7fd372af
Idel Fazletdinov <fvi-it@mail.ru>
2023-01-24 11:02:47 +0300

    Добавили меню

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers.user_handlers import register_user_handlers
 from handlers.other_handlers import register_other_handlers
-
+from keyboards.menu_button import set_main_menu
 
 # Инициализируем логгер
 logger = logging.getLogger(__name__)
@@ -33,7 +33,8 @@ async def main():
     # Инициализируем бот и диспетчер
     bot: Bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp: Dispatcher = Dispatcher(bot)
-
+    await set_main_menu(dp)
+    
     # Регистрируем все хэндлеры
     register_all_handlers(dp)
 
---
+++ keyboards/menu_button.py
@@ -0,0 +1,12 @@
+from aiogram import Dispatcher, types
+
+
+# Функция для настройки кнопки Menu бота
+async def set_main_menu(dp: Dispatcher):
+    main_menu_commands = [
+        types.BotCommand(command='/start', description="Нажмите чтобы начать играть"),
+        types.BotCommand(command='/help', description="Справка по работе бота"),
+
+    ]
+
+    await dp.bot.set_my_commands(main_menu_commands)
